f39582b367873018e0a0fc977e869ae4c39c5c8c
Dionescha <dioneluisschallenberger@hotmail.com>
2022-06-13 22:24:17 -0300

    a

--- src/Home.js
+++ src/Home.js
@@ -1,71 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import { useFormik } from 'formik';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Grid, Paper } from '@mui/material';
 
-function Example() {
+function Home() {
   const [temp, setTemp] = useState('');
   const [weather, setWeather] = useState();
-    useEffect(() => {
-    axios.get('https://goweather.herokuapp.com/weather/Curitiba')
 
-    .then((response) =>{
+  useEffect(() => {
+    axios.get('https://goweather.herokuapp.com/weather/Curitiba')
+      .then((response) => {
         setTemp(response.data.temperature)
         setWeather(response.data)
-    })
-  },[]);
+      })
+  }, []);
+
   const formik = useFormik({
     initialValues: {
-      email: '',
+      email: 'teste@teste.com',
+      senha: '',
     },
-    onSubmit: search, 
-     
+    onSubmit: search,
   });
-  const {handleChange, values, handleSubmit} = formik
-  const {email} = values
+
+  const { handleChange, values, handleSubmit } = formik
+  const { email, senha } = values
 
   function search(values) {
     alert(JSON.stringify(values, null, 2));
-
   }
-  
+
   return (
-    <form onSubmit={handleSubmit}>
-    
-    
-    <TextField
-    id="email"
-    name="email"
-    type="email"
-    onChange={handleChange}
-    value={email}
-     />
-    <Button
-     type="submit"
-     variant="contained"
-     >Submit</Button>
-  </form>
+    <>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          height: '100vh',
+        }}
+      >
+        <Grid item >
+          <form onSubmit={handleSubmit}>
+            <Paper
+              sx={{
+                padding: 3,
+              }}
+            >
+              <Grid
+                container
+                justifyContent="center"
+                spacing={2}
+                direction="column"
+              >
+                <Grid item>
+                  <TextField
+                    id="email"
+                    name="email"
+                    type="email"
+                    label="Email"
+                    onChange={handleChange}
+                    value={email}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="password"
+                    name="senha"
+                    type="password"
+                    label="Senha"
+                    onChange={handleChange}
+                    value={senha}
+                  />
+                </Grid>
+
+                <Grid item>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+            </Paper>
+          </form>
+        </Grid>
+      </Grid >
+    </>
   );
 }
 
-
-
-
-
-
-
-
-
-
-
-function Home() {
-    return <h1>Home</h1>
-}
-
-
-
 export function Teste() {
-    return <h1>Teste</h1>
+  return <h1>Teste</h1>
 }
 
-export default Example;
+export default Home
